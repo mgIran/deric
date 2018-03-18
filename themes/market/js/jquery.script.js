cfcae9440127d1e166d8402d148fc28ec88378aa
Yusef <yusef.mobasheri@gmail.com>
2018-03-18 18:00:21 +0330

    Panel Changes

--- themes/market/js/jquery.script.js
+++ themes/market/js/jquery.script.js
@@ -138,15 +138,31 @@ $(function () {
             onSet: null
         });
 
-    $(".nicescroll").each(function () {
-        var options = $(this).data();
+    if($(window).width() > 992) {
+        $(".nicescroll").each(function () {
+            var options = $(this).data();
 
-        $.each(options, function (key, value) {
-            if (typeof value == "string" && value.indexOf("js:") != -1)
-                options[key] = JSON.parse(value.substr(3));
+            $.each(options, function (key, value) {
+                if (typeof value == "string" && value.indexOf("js:") != -1)
+                    options[key] = JSON.parse(value.substr(3));
+            });
+
+            $(this).niceScroll(options);
         });
+    }
+    $(window).resize(function () {
+        if($(window).width() > 992) {
+            $(".nicescroll").each(function () {
+                var options = $(this).data();
 
-        $(this).niceScroll(options);
+                $.each(options, function (key, value) {
+                    if (typeof value == "string" && value.indexOf("js:") != -1)
+                        options[key] = JSON.parse(value.substr(3));
+                });
+
+                $(this).niceScroll(options);
+            });
+        }
     });
 });
 
